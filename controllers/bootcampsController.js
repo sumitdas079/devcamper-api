ab683d9bde4e061ee23c95d0e451d59e9aa4b901
Sumit Das <sumitdas0849@gmail.com>
2023-02-16 22:38:25 +0530

    advanced filtering with queries

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -1,68 +1,83 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const Bootcamp = require('../models/BootcampModel')
+const geocoder = require('../utils/Geocoder')
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = asyncHandler (async (req,res,next) => {
-        const bootcamps = await Bootcamp.find()
-        res.status(200).json({success:true, count:bootcamps.length, data:bootcamps})
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    //advanced filtering based using fields(queryStrings)
+    let query
+    let queryStr = JSON.stringify(req.query) //need to use query as a string to manipulate it
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `${match}`)
+    query = Bootcamp.find(JSON.parse(queryStr))
+
+    const bootcamps = await query
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
 
 })
 
 // @desc      Get single bootcamp
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
-exports.getBootcamp = asyncHandler (async (req,res,next) => {
-        const bootcamp = await Bootcamp.findById(req.params.id)
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`,404))
-        }
-        res.status(200).json({success:true, data:bootcamp})
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`, 404))
+    }
+    res.status(200).json({ success: true, data: bootcamp })
 })
 
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.createBootcamp = asyncHandler (async (req,res,next) => {
-        const bootcamp = await Bootcamp.create(req.body)
-        res.status(201).json({
-            success:true, data:bootcamp
-        })
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(201).json({
+        success: true, data: bootcamp
+    })
 })
 
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
-exports.updateBootcamp = asyncHandler (async (req,res,next) => {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`,404))
-        }
-        res.status(201).json({
-            success:true,data:bootcamp
-        })
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`, 404))
+    }
+    res.status(201).json({
+        success: true, data: bootcamp
+    })
 })
 
 // @desc      Delete bootcamp
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
-exports.deleteBootcamp = asyncHandler (async (req,res,next) => {
-        const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id)
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`,404))
-        }
-        res.status(200).json({success:true, data:{}})
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id)
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`, 404))
+    }
+    res.status(200).json({ success: true, data: {} })
 })
 
 // @desc      Get bootcamps in a radius
 // @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
 // @access    Private
-exports.getsBootcampsinRadius = asyncHandler (async (req,res,next) => {
-        const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id)
-        if(!bootcamp){
-            return next(new ErrorResponse(`Bootcamp not found, id: ${req.params.id}`,404))
-        }
-        res.status(200).json({success:true, data:{}})
+exports.getsBootcampsinRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params
+
+    //get lat/long from geocoder
+    const loc = await geocoder.geocode(zipcode)
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    //calc radius using radians
+    const radius = distance / 6378
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    })
+    res.status(200).json({ succes: true, count: bootcamps.length, data: bootcamps })
 })
--- routes/bootcampsRoutes.js
+++ routes/bootcampsRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp} = require('../controllers/bootcampsController')
+const { getBootcamp, getBootcamps, createBootcamp, updateBootcamp, deleteBootcamp, getsBootcampsinRadius } = require('../controllers/bootcampsController')
 
+router.route('/radius/:zipcode/:distance').get(getsBootcampsinRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
